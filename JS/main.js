d3a4bead4a62021c4d4f8eaea2848586cd9049ee
my name <my@email.com>
2023-02-27 21:53:54 +0500

    JS added

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+function darkmode() {
+  document.getElementById("headerr").style.backgroundColor = "black";
+  document.getElementById("wrapper2.2").style.backgroundColor = "black";
+  document.getElementById("wrapper3.3").style.backgroundColor = "black";
+}
+function lightmode() {
+  document.getElementById("headerr").style.backgroundColor = "white";
+  document.getElementById("wrapper2.2").style.backgroundColor = "white";
+  document.getElementById("wrapper3.3").style.backgroundColor = "white";
+}
